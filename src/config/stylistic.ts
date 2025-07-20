2913fd29375dd8148741366428f461f2c93bb59e
FlandreDaisuki <vbnm123c+github@gmail.com>
2025-07-20 11:32:23 +0800

    feat: rewrite config/stylistic format

--- src/config/stylistic.ts
+++ src/config/stylistic.ts
@@ -3,14 +3,16 @@ import stylistic from '@stylistic/eslint-plugin';
 export default [
   {
     name: 'flandre/stylistic/rules',
-    ...stylistic.configs.customize({
-      flat: true,
-      indent: 2,
-      jsx: true,
-      quotes: 'single',
-      semi: true,
-    }),
+    plugins: {
+      '@stylistic': stylistic,
+    },
     rules: {
+      ...stylistic.configs.customize({
+        indent: 2,
+        jsx: true,
+        quotes: 'single',
+        semi: true,
+      }).rules,
       '@stylistic/max-statements-per-line': ['error', { max: 2 }],
       '@stylistic/arrow-parens': ['error', 'always'],
     },
